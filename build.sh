4d5066f84061f61e9279b187a108c5a604394bbb
taylorchu <tailinchu@gmail.com>
2011-08-29 17:47:19 -0700

    Edited build.sh via GitHub

--- build.sh
+++ build.sh
@@ -41,8 +41,9 @@ make_customize_root_image() {
 	find ${work_dir}/root-image/usr/share/locale/* ! -name locale.alias | xargs rm -rf
 	find ${work_dir}/root-image/usr/share/ -name doc -type d | xargs rm -rf
 	find ${work_dir}/root-image/usr/share/ -name examples -type d | xargs rm -rf
+	find ${work_dir}/root-image/usr/share/i18n/locales/* ! -name en_US | xargs rm -rf
+	find ${work_dir}/root-image/usr/share/i18n/charmaps/* ! -name UTF-8.gz | xargs rm -rf
 	rm -rf ${work_dir}/root-image/usr/share/X11/locale/
-	rm -rf ${work_dir}/root-image/usr/share/i18n/
 	rm -rf ${work_dir}/root-image/usr/share/man/
 	rm -rf ${work_dir}/root-image/usr/share/gtk-doc/
 	rm -rf ${work_dir}/root-image/usr/share/licenses/
