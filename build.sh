f35e90783c827ede31d883c8252c5ef3a04349ee
taylorchu <tailinchu@gmail.com>
2011-08-30 08:16:56 -0700

    Edited build.sh via GitHub

--- build.sh
+++ build.sh
@@ -39,12 +39,11 @@ make_customize_root_image() {
 	sed -i -e "s|^DAEMONS=.*|DAEMONS=(dbus networkmanager gdm avahi-daemon cupsd)|" ${work_dir}/root-image/etc/rc.conf
 	# remove unused manual and locale
 	find ${work_dir}/root-image/usr/share/locale/* ! -name locale.alias | xargs rm -rf
-	find ${work_dir}/root-image/usr/share/ -name doc -type d | xargs rm -rf
-	find ${work_dir}/root-image/usr/share/ -name examples -type d | xargs rm -rf
 	find ${work_dir}/root-image/usr/share/i18n/locales/* ! -name en_US ! -name en_GB ! -name i18n ! -name iso14651_t1* ! -name translit_* | xargs rm -rf
 	find ${work_dir}/root-image/usr/share/i18n/charmaps/* ! -name UTF-8.gz | xargs rm -rf
 	rm -rf ${work_dir}/root-image/usr/share/X11/locale/
 	rm -rf ${work_dir}/root-image/usr/share/man/
+	rm -rf ${work_dir}/root-image/usr/share/doc/
 	rm -rf ${work_dir}/root-image/usr/share/gtk-doc/
 	rm -rf ${work_dir}/root-image/usr/share/licenses/
 	rm -rf ${work_dir}/root-image/usr/share/kbd/locale/
