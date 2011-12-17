d60d08e9b4431f6d1b4e7d9ff492cb8ffb98dbbd
taylorchu <tailinchu@gmail.com>
2011-12-16 17:05:33 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,9 +32,7 @@ make_customize_root_image() {
 	sed -i -e 's|^HARDWARECLOCK=.*|HARDWARECLOCK="localtime"|' \
 	-e 's|^HOSTNAME=.*|HOSTNAME="localhost"|' \
 	-e 's|^DAEMONS=.*|DAEMONS=(dbus @alsa @wicd @cupsd)|' ${work_dir}/root-image/etc/rc.conf
-	# remove unused manual and locale
-	find ${work_dir}/root-image/usr/share/locale/* ! -name locale.alias | xargs rm -rf
-	rm -rf ${work_dir}/root-image/usr/share/X11/locale/
+	# remove unused manual
 	rm -rf ${work_dir}/root-image/usr/share/man/
 	rm -rf ${work_dir}/root-image/usr/share/doc/
 	rm -rf ${work_dir}/root-image/usr/share/gtk-doc/
