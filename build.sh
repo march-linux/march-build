e5e636a13f1212c2231d9cd5651d10c6be79603f
taylorchu <march@localhost.localdomain>
2012-03-14 02:51:51 +0000

    font problem

--- build.sh
+++ build.sh
@@ -83,7 +83,7 @@ make_syslinux() {
             s|%INSTALL_DIR%|${install_dir}|g;
             s|%ARCH%|${arch}|g" ${script_path}/syslinux/syslinux.cfg > ${work_dir}/iso/${install_dir}/boot/syslinux/syslinux.cfg
 	convert ${work_dir}/root-image/etc/skel/.wallpaper/wallpaper.jpg -resize 640x480^ -gravity center -extent 640x480 \
-		-fill white -pointsize 12 -draw "text 250,220 '${iso_version}-${arch}'" \
+		-font Droid-Sans-Mono-Regular -fill white -pointsize 12 -draw "text 250,220 '${iso_version}-${arch}'" \
 		${work_dir}/iso/${install_dir}/boot/syslinux/splash.jpg
         cp ${work_dir}/root-image/usr/lib/syslinux/{vesamenu.c32,chain.c32,reboot.c32,poweroff.com} ${work_dir}/iso/${install_dir}/boot/syslinux/
         : > ${work_dir}/build.${FUNCNAME}
