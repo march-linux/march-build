d51b259ab8a3a60d6c4fc56f910f335edc6e3292
taylorchu <march@localhost.localdomain>
2012-03-27 21:03:13 +0000

    update build dep

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e -u
-# dep: make imagemagick devtools squashfs-tools libisoburn
+# dep: make imagemagick ttf-droid squashfs-tools libisoburn
 
 iso_name=march
 iso_label="MARCH_$(date +%Y%m)"
